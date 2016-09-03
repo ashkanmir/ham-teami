c9bfc9e047dc27678edeed05a9b25e01f9847725
Ash Miri <miri.ashkan@gmail.com>
2016-09-03 16:25:17 +0000

    title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,22 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
  
-
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "هم تیمی"
+  end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    
+    assert_select "title", "هم تیمی"
   end
   
   test "should get contactus" do
     get static_pages_contactus_url
     assert_response :success
-    
+    assert_select "title", "هم تیمی"
   end
 
 end
